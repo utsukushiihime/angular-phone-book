cc44fb9f1ac053b3ffae5c95b4309cc2e0e8674d
creattech <crystalsmcneil@gmail.com>
2020-12-14 04:18:36 -0600

    adds values to table

--- phone-book/src/app/main-app/main-app.component.ts
+++ phone-book/src/app/main-app/main-app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,15 +7,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   template: `
     <div>
       <form [formGroup]="registerForm" (ngSubmit)="onSubmit()">
-          <div class="form-row">
-            <div class="form-group col-5">
+          <div>
+            <div>
               <label>First Name</label>
               <input type="text" formControlName="firstName" name="firstName"  id="firstName" class="userFirstname" [ngClass]="{ 'is-invalid': submitted && f.firstName.errors }" />
               <div *ngIf="submitted && f.firstName.errors" class="invalid-feedback">
                 <div *ngIf="f.firstName.errors.required">First Name is required</div>
               </div>
             </div>
-            <div class="form-group col-5">
+            <div>
               <label>Last Name</label>
               <input type="text" formControlName="lastName" name="lastName" id="lastName" class="userLastname" [ngClass]="{ 'is-invalid': submitted && f.lastName.errors }" />
               <div *ngIf="submitted && f.lastName.errors" class="invalid-feedback">
@@ -23,7 +23,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
               </div>
             </div>
           </div>
-          <div class="form-row">
+          <div>
             <div>
               <label>Phone</label>
               <input type="tel" formControlName="phone" name="phoneNumber" id="phoneNumber" class="userPhone" [ngClass]="{ 'is-invalid': submitted && f.phone.errors }" />
@@ -34,7 +34,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
             </div>
           </div>
           <div class="text-center">
-            <button class="btn btn-primary mr-1">Register</button>
+            <button value="submit" class="submitButton">Submit</button>
             <button class="btn btn-secondary" type="reset" (click)="onReset()">Cancel</button>
           </div>
       </form>
@@ -50,10 +50,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>{{ firstName }}</td>
-          <td>{{ lastName }}</td>
-          <td>{{ phone }}</td>
+        <tr *ngFor="let entry of entries" >
+          <td>{{ entry.firstName }}</td>
+          <td>{{ entry.lastName }}</td>
+          <td>{{ entry.phone }}</td>
         </tr>
         </tbody>
       </table>
@@ -79,14 +79,14 @@ export class MainAppComponent implements OnInit {
 
   get f() { return this.registerForm.controls; }
 
+  entries = [];
   onSubmit() {
     this.submitted = true;
 
     if (this.registerForm.invalid) {
       return;
     }
-
-    alert( JSON.stringify(this.registerForm.value, null, 4));
+    this.entries.push(this.registerForm.value);
   }
 
   onReset() {
